a5fac2d8f782e46b1f19adfc280f402fe4e4c7c9
Mate Antunovic <mateATantunovic.nz>
2020-02-08 23:00:29 +1300

    Make windows work

--- src/tests/window_tests.rs
+++ src/tests/window_tests.rs
@@ -1,6 +1,7 @@
 use crate::time_window::TimeWindow;
 use chrono::Duration;
-use chrono::offset::Utc;
+use chrono::{NaiveDate, DateTime, TimeZone, NaiveDateTime, Utc};
+
 
 #[test]
 fn creates_instant() {
@@ -21,4 +22,16 @@ fn has_after_next_window() {
     let next_time_window = TimeWindow::new(Utc::now() + Duration::minutes(1), Duration::minutes(1));
 
     assert!(next_time_window.is_after_next(&time_window));
+}
+
+#[test] 
+fn window_tests() {
+    let now = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2016, 7, 8).and_hms(9, 10, 0), Utc);
+    let just_a_bit_later = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2016, 7, 8).and_hms(9, 11, 0), Utc);
+    let quite_a_bit_later = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2016, 7, 8).and_hms(9, 16, 0), Utc);
+
+    let window_duration = Duration::minutes(5);
+
+    assert_eq!(TimeWindow::from(now, window_duration), TimeWindow::from(just_a_bit_later, window_duration));
+    assert_ne!(TimeWindow::from(now, window_duration), TimeWindow::from(quite_a_bit_later, window_duration));
 }
--- src/time_window.rs
+++ src/time_window.rs
@@ -2,36 +2,52 @@ use std::cmp::{Eq, PartialEq};
 use chrono::Duration;
 use chrono::DateTime;
 use chrono::offset::Utc;
+use chrono::NaiveDateTime;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TimeWindow {
     start: DateTime<Utc>,
-    length: Duration
+    duration: Duration
 }
 
 impl TimeWindow {
-    pub fn new(start: DateTime<Utc>, length: Duration) -> Self {
+    pub fn new(start: DateTime<Utc>, duration: Duration) -> Self {
         Self {
             start,
-            length
+            duration
+        }
+    }
+
+    pub fn from(instance: DateTime<Utc>, duration: Duration) -> Self {
+        const NANOSECONDS_IN_MILLISECOND: i64 = 1000000;
+        // convert to millis
+        let instance_millis = instance.timestamp_millis();
+        let duration_millis = duration.num_milliseconds();
+        let window_millis = instance_millis - (instance_millis % duration_millis);
+        let window_nanosecond_part = window_millis % (1000 * 1000);
+        let start = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(window_millis / 1000, 0), Utc);
+
+        Self {
+            start,
+            duration
         }
     }
 
     pub fn contains(&self, instant: DateTime<Utc>) -> bool {
-        return self.start <= instant && instant < self.start + self.length;
+        return self.start <= instant && instant < self.start + self.duration;
     }
 
     pub fn is_next(&self, other: &TimeWindow) -> bool {
-        self.start >= other.start + other.length
+        self.start >= other.start + other.duration
     }
 
     pub fn is_after_next(&self, other: &TimeWindow) -> bool {
-        self.start + self.length >= other.start
+        self.start + self.duration >= other.start
     }
 
     pub fn slide_ratio(&self, other: &DateTime<Utc>) -> f64 {
         let instance_milliseconds = f64::from(other.signed_duration_since(self.start.clone()).num_milliseconds() as i32);
-        let window_milliseconds = f64::from(self.length.num_milliseconds() as i32);
+        let window_milliseconds = f64::from(self.duration.num_milliseconds() as i32);
 
         instance_milliseconds / window_milliseconds
     }
