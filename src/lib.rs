9dc7a6a47e0140e057860650cf4122d8f1b8e3f9
Mate Antunovic <mateATantunovic.nz>
2020-02-08 22:11:50 +1300

    Export rate limit structs

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,10 @@ mod rate_limit_store;
 mod rate_limit_strategy;
 mod sliding_window_rate_limit_strategy;
 
+pub use time_window::TimeWindow;
+pub use rate_limit_store::RateLimitStore;
+pub use rate_limit_strategy::RateLimitStrategy;
+pub use sliding_window_rate_limit_strategy::SlidingWindowRateLimitStrategy;
+
 #[cfg(test)]
 mod tests;
